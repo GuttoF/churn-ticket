da42fe30a64a2cac7af3b45b0b6daf2a442532cc
Gutto <guttolaudie@gmail.com>
2024-08-22 21:18:33 -0300

    fix: Deploy pt.12

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-exec poetry run streamlit run src/app/main.py --server.port="$PORT"
+if ! [ -z "$PORT" ] && [ "$PORT" -eq "$PORT" ] 2>/dev/null; then
+    exec poetry run streamlit run src/app/main.py --server.port=$PORT
+else
+    echo "PORT variable is not set or not a valid integer"
+    exit 1
+fi
