b9726c047b9bab5a4b963b5140127a8af238f32e
Gutto <guttolaudie@gmail.com>
2024-08-23 19:46:06 -0300

    fix: deploy api railway speed

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from .routes import router
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI(
     title="Churn Prediction API",
     description="API for predicting customer churn using a machine learning model",
     version="1.0.0",
 )
+
+# Add GZip middleware
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 # Include the router
 app.include_router(router)
